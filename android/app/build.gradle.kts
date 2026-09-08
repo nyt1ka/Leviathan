6a51f642a32e013035d6891cf6bf10de91a230db
nyt1ka <117991409+nyt1ka@users.noreply.github.com>
2026-09-08 11:19:28 +0400

    Remove kotlinOptions for jvmTarget in build.gradle.kts
    Removed kotlinOptions configuration for jvmTarget.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,10 +31,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     packaging {
         resources.excludes += setOf("META-INF/AL2.0", "META-INF/LGPL2.1")
     }
