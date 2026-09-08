8c23b322e0d20966ed70b8b8fcdaeedbe9b66b9e
nyt1ka <117991409+nyt1ka@users.noreply.github.com>
2026-09-08 15:32:45 +0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,11 +19,23 @@ android {
     }
 
     buildFeatures {
-        compose = true
-    }
+    compose = true
+}
 
-    packaging {
-        resources {
+compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlinOptions {
+    jvmTarget = "17"
+}
+
+packaging {
+    resources {
+        excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    }
+}
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
