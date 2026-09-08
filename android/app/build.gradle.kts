463aa981132e7524bdd13ca53678f1623e926600
nyt1ka <117991409+nyt1ka@users.noreply.github.com>
2026-09-08 11:21:25 +0400

    Downgrade activity-compose version to 1.13.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2026.08.00"))
-    implementation("androidx.activity:activity-compose:1.14.0")
+    implementation("androidx.activity:activity-compose:1.13.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
