119dbd28717e00b7f74df0f14476a4c4469c2bbf
nyt1ka <117991409+nyt1ka@users.noreply.github.com>
2026-09-08 15:42:46 +0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,23 +19,20 @@ android {
     }
 
     buildFeatures {
-    compose = true
-}
-
-compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
-kotlinOptions {
-    jvmTarget = "17"
-}
-
-packaging {
-    resources {
-        excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        compose = true
     }
-}
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    packaging {
+        resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
@@ -43,12 +40,16 @@ packaging {
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+
     implementation("androidx.activity:activity-compose:1.10.0")
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+
     implementation("androidx.navigation:navigation-compose:2.8.5")
+
     implementation("io.coil-kt:coil-compose:2.7.0")
 
     testImplementation("junit:junit:4.13.2")
